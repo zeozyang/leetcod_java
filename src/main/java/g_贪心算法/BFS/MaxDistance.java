0f712367a28e953199c010b7ac4d4e893360fb15
yangb <1337738378@qq.com>
2022-02-11 10:11:38 +0800

    重写了多元bfs一题。

--- src/main/java/g_贪心算法/BFS/MaxDistance.java
+++ src/main/java/g_贪心算法/BFS/MaxDistance.java
@@ -1,9 +1,6 @@
 package g_贪心算法.BFS;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author yang 2022-01-29 15:36
@@ -11,33 +8,31 @@ import java.util.Map;
 public class MaxDistance {
     public int maxDistance(int[][] grid) {
         int n = grid.length;
-        Deque<int[]> d = new ArrayDeque<>();
-        Map<Integer, Integer> map = new HashMap<>(); // 格子对应的离陆地的距离
-        // 先把所有的陆地都入队，因为图可以有多个源点，所以首先需要把多个源点都入队
+        Deque<int[]> deque = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>(); //每个格子对应的离陆地距离
+        // 把所有源点如队列
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    d.add(new int[]{i, j});
+                    deque.addLast(new int[]{i, j});
                     map.put(i * n + j, 0);
                 }
             }
         }
 
-        // 从各个陆地开始，一圈一圈的遍历海洋，最后遍历到的海洋就是离陆地最远的海洋。
         int ans = -1;
         int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!d.isEmpty()) {
-            int[] poll = d.pollFirst();
+        while (!deque.isEmpty()) {
+            int[] poll = deque.pollFirst();
             int dx = poll[0], dy = poll[1];
-            int step = map.get(dx * n + dy);
             for (int[] dir : dirs) {
                 int nx = dx + dir[0], ny = dy + dir[1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; //如果出界就直接换方向
                 if (grid[nx][ny] != 0) continue;
-                grid[nx][ny] = step + 1;
-                d.addLast(new int[]{nx, ny});
-                map.put(nx * n + ny, step + 1);
-                ans = Math.max(ans, step + 1);
+                grid[nx][ny] = map.get(dx * n + dy) + 1;
+                ans = Math.max(ans, grid[nx][ny]);
+                deque.addLast(new int[]{nx, ny});
+                map.put(nx * n + ny, grid[nx][ny]);
             }
         }
         return ans;
@@ -47,4 +42,5 @@ public class MaxDistance {
 /*
 1162. 地图分析
 https://leetcode-cn.com/problems/as-far-from-land-as-possible/
+你现在手里有一份大小为 n x n 的 网格 grid，上面的每个 单元格 都用 0 和 1 标记好了。其中 0 代表海洋，1 代表陆地，请你找出一个海洋单元格，这个海洋单元格到离它最近的陆地单元格的距离是最大的。如果网格上只有陆地或者海洋，请返回 -1。
  */
