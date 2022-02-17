41133dad621d0db4c758122a078069bec62bc20f
yangb <1337738378@qq.com>
2022-02-17 16:02:52 +0800

    完成22/2/17每日一题

--- src/main/java/每日一题/KnightProbability.java
+++ src/main/java/每日一题/KnightProbability.java
@@ -8,17 +8,19 @@ public class KnightProbability {
 
     public double knightProbability(int n, int k, int row, int column) {
         double[][][] dp = new double[n][n][k + 1];
+        //base case
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 dp[i][j][0] = 1;
             }
         }
+
         for (int p = 1; p <= k; p++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     for (int[] dir : dirs) {
                         int nx = i + dir[0], ny = j + dir[1];
-                        if (nx < 1 || nx >= n || ny < 0 || ny >= n) continue;
+                        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
                         dp[i][j][p] += dp[nx][ny][p - 1] / 8;
                     }
                 }
