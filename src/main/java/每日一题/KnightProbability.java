96bd5adbe1af7305712b0594f64686dc69a97701
yangb <1337738378@qq.com>
2022-02-17 16:00:15 +0800

    完成22/2/17每日一题

---
+++ src/main/java/每日一题/KnightProbability.java
@@ -0,0 +1,51 @@
+package 每日一题;
+
+/**
+ * @author yang 2022-02-17 9:07
+ */
+public class KnightProbability {
+    int[][] dirs = new int[][]{{-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {-2, 1}, {-2, -1}, {2, 1}, {2, -1}};
+
+    public double knightProbability(int n, int k, int row, int column) {
+        double[][][] dp = new double[n][n][k + 1];
+        //base case
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j][0] = 1;
+            }
+        }
+
+        for (int p = 1; p <= k; p++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int[] dir : dirs) {
+                        int nx = i + dir[0], ny = j + dir[1];
+                        if (nx < 1 || nx >= n || ny < 0 || ny >= n) continue;
+                        dp[i][j][p] += dp[nx][ny][p - 1] / 8;
+                    }
+                }
+            }
+        }
+        return dp[row][column][k];
+    }
+}
+/*
+688. 骑士在棋盘上的概率
+在一个 n x n 的国际象棋棋盘上，一个骑士从单元格 (row, column) 开始，并尝试进行 k 次移动。行和列是 从 0 开始 的，所以左上单元格是 (0,0) ，右下单元格是 (n - 1, n - 1) 。
+象棋骑士有8种可能的走法，如下图所示。每次移动在基本方向上是两个单元格，然后在正交方向上是一个单元格。
+
+每次骑士要移动时，它都会随机从8种可能的移动中选择一种(即使棋子会离开棋盘)，然后移动到那里。
+骑士继续移动，直到它走了 k 步或离开了棋盘。
+返回 骑士在棋盘停止移动后仍留在棋盘上的概率 。
+
+示例 1：
+输入: n = 3, k = 2, row = 0, column = 0
+输出: 0.0625
+解释: 有两步(到(1,2)，(2,1))可以让骑士留在棋盘上。
+在每一个位置上，也有两种移动可以让骑士留在棋盘上。
+骑士留在棋盘上的总概率是0.0625。
+
+示例 2：
+输入: n = 1, k = 0, row = 0, column = 0
+输出: 1.00000
+ */
