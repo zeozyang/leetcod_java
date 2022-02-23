79a4d7164f726f388dc5fd6b76a4e085a372af5d
yangb <1337738378@qq.com>
2022-02-23 10:07:27 +0800

    完成22/2/23每日一题

---
+++ src/main/java/a_数组和字符串/字符串/ReverseOnlyLetters.java
@@ -0,0 +1,51 @@
+package a_数组和字符串.字符串;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author yang 2022-02-23 8:54
+ */
+public class ReverseOnlyLetters {
+    public String reverseOnlyLetters(String s) { //前后双指针
+        char[] chars = s.toCharArray();
+        StringBuilder charList = new StringBuilder();
+        Deque<Integer> indexList = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
+                charList.append(s.charAt(i));
+                indexList.addLast(i);
+            }
+        }
+        StringBuilder reverse = charList.reverse();
+        int stringIndex = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (!indexList.isEmpty() && i == indexList.peekFirst()) {
+                indexList.removeFirst();
+                chars[i] = reverse.charAt(stringIndex);
+                stringIndex++;
+            }
+        }
+        return String.valueOf(chars);
+    }
+}
+
+/*
+917. 仅仅反转字母
+给你一个字符串 s ，根据下述规则反转字符串：
+所有非英文字母保留在原有位置。
+所有英文字母（小写或大写）位置反转。
+返回反转后的 s 。
+
+示例 1：
+输入：s = "ab-cd"
+输出："dc-ba"
+
+示例 2：
+输入：s = "a-bC-dEf-ghIj"
+输出："j-Ih-gfE-dCba"
+
+示例 3：
+输入：s = "Test1ng-Leet=code-Q!"
+输出："Qedo1ct-eeLg=ntse-T!"
+ */
---
+++ src/main/java/a_数组和字符串/简单/TowSum.java
@@ -0,0 +1,49 @@
+package a_数组和字符串.简单;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yang 2022-02-22 16:17
+ */
+public class TowSum {
+    public int[] twoSum(int[] nums, int target) { // On^2
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int[] twoSum1(int[] nums, int target) { // On
+        Map<Integer, Integer> map = new HashMap<>(); //每个数对应的下标。
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
+
+/*
+1. 两数之和
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+你可以按任意顺序返回答案。
+
+示例 1：
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+示例 2：
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+
+示例 3：
+输入：nums = [3,3], target = 6
+输出：[0,1]
+ */
